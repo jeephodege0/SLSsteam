0aca5ba41a6816470aa056d1be90a98b71f22298
PSSGAMER <prsnl.pss+github@gmail.com>
2025-10-06 16:17:55 +0530

    Refactor

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -146,95 +146,6 @@ static void hkLogSteamPipeCall(const char* iface, const char* fn)
 static bool applistRequested = false;
 static auto appIdOwnerOverride = std::map<uint32_t, int>();
 
-static bool FASTCALL_TYPE hkCheckAppOwnership(void* pThis, void* /*edx_dummy*/, uint32_t appId, CAppOwnershipInfo* pOwnershipInfo)
-{
-	const bool ret = Hooks::CheckAppOwnership.tramp.fn(pThis, appId, pOwnershipInfo);
-
-	//Do not log pOwnershipInfo because it gets deleted very quickly, so it's pretty much useless in the logs
-	g_pLog->once("CheckAppOwnership(%p, %u) -> %i", pThis, appId, ret);
-
-	//Wait Until GetSubscribedApps gets called once to let Steam request and populate legit data first.
-	//Afterwards modifying should hopefully not affect false positives anymore
-	if (!applistRequested || g_config.shouldExcludeAppId(appId) || !pOwnershipInfo || !g_currentSteamId)
-	{
-		return ret;
-	}
-	
-	const uint32_t denuvoOwner = g_config.getDenuvoGameOwner(appId);
-	//Do not modify Denuvo enabled Games
-	if (!g_config.denuvoSpoof && denuvoOwner && denuvoOwner != g_currentSteamId) 
-	{
-		//Would love to log the SteamId, but for users anonymity I won't
-		g_pLog->once("Skipping %u because it's a Denuvo game from someone else", appId);
-		return ret;
-	}
-
-	if (g_config.isAddedAppId(appId) || (g_config.playNotOwnedGames && !pOwnershipInfo->purchased))
-	{
-		if (!denuvoOwner || denuvoOwner == g_currentSteamId)
-		{
-			//Changing the purchased field is enough, but just for nicety in the Steamclient UI we change the owner too
-			pOwnershipInfo->ownerSteamId = g_currentSteamId;
-			pOwnershipInfo->familyShared = false;
-		}
-		else if (denuvoOwner)
-		{
-			pOwnershipInfo->ownerSteamId = denuvoOwner;
-			pOwnershipInfo->familyShared = true;
-		}
-
-		pOwnershipInfo->purchased = true;
-		//Unnessecary but whatever
-		pOwnershipInfo->permanent = true;
-
-		//Found in backtrace
-		pOwnershipInfo->releaseState = 4;
-		pOwnershipInfo->field10_0x25 = 0;
-		//Seems to do nothing in particular, some dlc have this as 1 so I uncomented this for now. Might be free stuff?
-		//pOwnershipInfo->field27_0x36 = 1;
-
-		g_config.addAdditionalAppId(appId);
-	}
-
-	//Doing that might be not worth it since this will most likely be easier to mantain
-	//TODO: Backtrace those 4 calls and only patch the really necessary ones since this might be prone to breakage
-	if (!denuvoOwner && g_config.disableFamilyLock && appIdOwnerOverride.count(appId) && appIdOwnerOverride.at(appId) < 4)
-	{
-		pOwnershipInfo->ownerSteamId = 1; //Setting to "arbitrary" steam Id instead of own, otherwise bypass won't work for own games
-		//Unnessecarry again, but whatever
-		pOwnershipInfo->permanent = true;
-		pOwnershipInfo->familyShared = false;
-
-		appIdOwnerOverride[appId]++;
-	}
-
-	//Returning false after we modify data shouldn't cause any problems because it should just get discarded
-
-	if (!g_pClientApps)
-		return ret;
-
-	auto type = g_pClientApps->getAppType(appId);
-	if (type == APPTYPE_DLC) //Don't touch DLC here, otherwise downloads might break. Hopefully this won't decrease compatibility
-	{
-		return ret;
-	}
-
-	if (g_config.automaticFilter)
-	{
-		switch(type)
-		{
-			case APPTYPE_APPLICATION:
-			case APPTYPE_GAME:
-				break;
-
-			default:
-				return ret;
-		}
-	}
-
-	return true;
-}
-
 void* FASTCALL_TYPE hkClientAppManager_LaunchApp(void* pClientAppManager, void* /*edx_dummy*/, uint32_t* pAppId, void* a2, void* a3, void* a4)
 {
 	if (pAppId)
@@ -423,6 +334,96 @@ bool FASTCALL_TYPE hkClientUser_BIsSubscribedApp(void* pClientUser, void* /*edx_
 
 	return true;
 }
+
+static bool FASTCALL_TYPE hkCheckAppOwnership(void* pThis, void* /*edx_dummy*/, uint32_t appId, CAppOwnershipInfo* pOwnershipInfo)
+{
+	const bool ret = Hooks::CheckAppOwnership.tramp.fn(pThis, appId, pOwnershipInfo);
+
+	//Do not log pOwnershipInfo because it gets deleted very quickly, so it's pretty much useless in the logs
+	g_pLog->once("CheckAppOwnership(%p, %u) -> %i", pThis, appId, ret);
+
+	//Wait Until GetSubscribedApps gets called once to let Steam request and populate legit data first.
+	//Afterwards modifying should hopefully not affect false positives anymore
+	if (!applistRequested || g_config.shouldExcludeAppId(appId) || !pOwnershipInfo || !g_currentSteamId)
+	{
+		return ret;
+	}
+	
+	const uint32_t denuvoOwner = g_config.getDenuvoGameOwner(appId);
+	//Do not modify Denuvo enabled Games
+	if (!g_config.denuvoSpoof && denuvoOwner && denuvoOwner != g_currentSteamId) 
+	{
+		//Would love to log the SteamId, but for users anonymity I won't
+		g_pLog->once("Skipping %u because it's a Denuvo game from someone else", appId);
+		return ret;
+	}
+
+	if (g_config.isAddedAppId(appId) || (g_config.playNotOwnedGames && !pOwnershipInfo->purchased))
+	{
+		if (!denuvoOwner || denuvoOwner == g_currentSteamId)
+		{
+			//Changing the purchased field is enough, but just for nicety in the Steamclient UI we change the owner too
+			pOwnershipInfo->ownerSteamId = g_currentSteamId;
+			pOwnershipInfo->familyShared = false;
+		}
+		else if (denuvoOwner)
+		{
+			pOwnershipInfo->ownerSteamId = denuvoOwner;
+			pOwnershipInfo->familyShared = true;
+		}
+
+		pOwnershipInfo->purchased = true;
+		//Unnessecary but whatever
+		pOwnershipInfo->permanent = true;
+
+		//Found in backtrace
+		pOwnershipInfo->releaseState = 4;
+		pOwnershipInfo->field10_0x25 = 0;
+		//Seems to do nothing in particular, some dlc have this as 1 so I uncomented this for now. Might be free stuff?
+		//pOwnershipInfo->field27_0x36 = 1;
+
+		g_config.addAdditionalAppId(appId);
+	}
+
+	//Doing that might be not worth it since this will most likely be easier to mantain
+	//TODO: Backtrace those 4 calls and only patch the really necessary ones since this might be prone to breakage
+	if (!denuvoOwner && g_config.disableFamilyLock && appIdOwnerOverride.count(appId) && appIdOwnerOverride.at(appId) < 4)
+	{
+		pOwnershipInfo->ownerSteamId = 1; //Setting to "arbitrary" steam Id instead of own, otherwise bypass won't work for own games
+		//Unnessecarry again, but whatever
+		pOwnershipInfo->permanent = true;
+		pOwnershipInfo->familyShared = false;
+
+		appIdOwnerOverride[appId]++;
+	}
+
+	//Returning false after we modify data shouldn't cause any problems because it should just get discarded
+
+	if (!g_pClientApps)
+		return ret;
+
+	auto type = g_pClientApps->getAppType(appId);
+	if (type == APPTYPE_DLC) //Don't touch DLC here, otherwise downloads might break. Hopefully this won't decrease compatibility
+	{
+		return ret;
+	}
+
+	if (g_config.automaticFilter)
+	{
+		switch(type)
+		{
+			case APPTYPE_APPLICATION:
+			case APPTYPE_GAME:
+				break;
+
+			default:
+				return ret;
+		}
+	}
+
+	return true;
+}
+
 
 uint8_t FASTCALL_TYPE hkClientUser_IsUserSubscribedAppInTicket(void* pClientUser, void* /*edx_dummy*/, uint32_t steamId, uint32_t a2, uint32_t a3, uint32_t appId)
 {
